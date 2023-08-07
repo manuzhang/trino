1f8ec5b54c9d63550f8e0b3dd0ba561d4118ee7e
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-08-07 17:57:21 +0200

    Remove dead code in TrinoViewUtil.getView
    The check above excludes everything that does not look like a
    Presto/Trino view, so the second check looks redundant.
    
    This also makes one method parameter redundant, removed.

--- plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/glue/TrinoGlueCatalog.java
+++ plugin/trino-iceberg/src/main/java/io/trino/plugin/iceberg/catalog/glue/TrinoGlueCatalog.java
@@ -631,7 +631,7 @@ public class TrinoGlueCatalog
             }
 
             try {
-                TrinoViewUtil.getView(schemaTableName,
+                TrinoViewUtil.getView(
                                 Optional.ofNullable(table.getViewOriginalText()),
                                 getTableType(table),
                                 parameters,
@@ -849,7 +849,6 @@ public class TrinoGlueCatalog
         }
         com.amazonaws.services.glue.model.Table viewDefinition = table.get();
         return TrinoViewUtil.getView(
-                viewName,
                 Optional.ofNullable(viewDefinition.getViewOriginalText()),
                 getTableType(viewDefinition),
                 getTableParameters(viewDefinition),
